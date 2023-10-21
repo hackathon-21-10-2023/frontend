22b54da1bd2619143bf251c11d96a16aadea865b
Ilya-Zukhbaya <ne_perepletov@mail.ru>
2023-10-21 19:17:41 +0300

    login page styles ready

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 export const Form = () => {
-  return <div>Test</div>;
+  return (
+    <div className={styles.root}>
+      <h2>вход в систему</h2>
+      <div className={styles.root__form}>
+        <label>
+          Логин
+          <input type="email" name="email" id="em" placeholder="your.email@greensight.ru" />
+          <span></span>
+        </label>
+        <label>
+          Пароль
+          <input type="password" name="password" id="pass" />
+          <span></span>
+        </label>
+      </div>
+      <div className={styles.root__footer}>
+        <button type="button">Войти</button>
+      </div>
+    </div>
+  );
 };
