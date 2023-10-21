9ebb7a0b48ca0933ef79f41f7d2c63986bfa5789
Ilya-Zukhbaya <ne_perepletov@mail.ru>
2023-10-21 17:50:11 +0300

    working on LoginPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './scss/app.scss';
+import { Feedback } from './pages/Feedback';
+import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
+import { Result } from './pages/Result';
+import { Workers } from './pages/Workers';
 
 export const App = () => {
   return (
-    <div className="App">
-      <div>Hell0</div>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/workers" element={<Workers />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
