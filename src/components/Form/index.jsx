39d882abe53d2e51b6c3217a2b69ce511bcf394d
Ilya-Zukhbaya <ne_perepletov@mail.ru>
2023-10-22 15:21:45 +0300

    AUTH READY

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,26 +1,65 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export const Form = () => {
   const nav = useNavigate();
+
+  const [inputOne, setInputOne] = React.useState('');
+  const [inputTwo, setInputTwo] = React.useState('');
+
+  const onBtnClick = () => {
+    let loginState = {
+      login: inputOne,
+      pass: inputTwo,
+    };
+
+    axios
+      .post('https://hackathon-21-10-2023.adoge.ru/auth/login/', {
+        login: loginState.login,
+        password: loginState.pass,
+      })
+      .then(function (response) {
+        localStorage.setItem('jwt', JSON.stringify(response.data.token));
+        nav('/profile');
+      })
+      .catch(function (error) {
+        console.log(error);
+        nav('/');
+      });
+  };
+
   return (
     <div className={styles.root}>
       <h2>вход в систему</h2>
       <div className={styles.root__form}>
         <label>
           Логин
-          <input type="email" name="email" id="em" placeholder="your.email@greensight.ru" />
+          <input
+            value={inputOne}
+            type="email"
+            name="email"
+            id="em"
+            placeholder="your.email@greensight.ru"
+            onChange={(event) => setInputOne(event.target.value)}
+          />
           <span></span>
         </label>
         <label>
           Пароль
-          <input type="password" name="password" id="pass" />
+          <input
+            value={inputTwo}
+            type="password"
+            name="password"
+            id="pass"
+            onChange={(event) => setInputTwo(event.target.value)}
+          />
           <span></span>
         </label>
       </div>
       <div className={styles.root__footer}>
-        <button type="button" onClick={() => nav('/profile')}>
+        <button type="submit" onClick={onBtnClick}>
           Войти
         </button>
       </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Form } from '../components/Form';
 import { Header } from '../components/Header';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem('jwt')) {
+      navigate('/profile');
+    }
+  }, []);
   return (
     <div className="login_container">
       <Header />
